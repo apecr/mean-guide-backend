a50f51e1aa77c81a20d8dab37e73a8fba282be00
apecr <alberto.eyo@gmail.com>
2020-04-15 10:47:09 +0200

    feat() Include get path

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mean-guide-backend
+
+## Resources
+
+* Learn Node + Express from Scratch (for free!): https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs
+* Creating a REST API with Node + Express (+ MongoDB): https://academind.com/learn/node-js/building-a-restful-api-with/
--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT')
   next()
 })
 
-app.use('/api/posts', (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body
+  console.log(post)
+  res.status(201).json({
+    message: 'Post added successfully'
+  })
+})
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: 'assfdfb',
@@ -26,7 +38,7 @@ app.use('/api/posts', (req, res, next) => {
     }
   ]
   res.status(200).json({
-    message: 'Post sent successfully',
+    message: 'Posts fetched successfully',
     posts
   })
 })
