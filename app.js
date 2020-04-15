1c21e869e1b778c3813d215924090dd5d7584063
apecr <alberto.eyo@gmail.com>
2020-04-15 10:11:08 +0200

    feat(app.js) Include /api/posts
    First path to our express app

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('First middleware')
-  next()
-})
-
-app.use((req, res, next) => {
-  res.send('Hello from express!')
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 1,
+      title: 'Title from api 1',
+      content: 'Content from api 1'
+    }, {
+      id: 2,
+      title: 'Title from api 2',
+      content: 'Content from api 2'
+    }, {
+      id: 3,
+      title: 'Title from api 3',
+      content: 'Content from api 3'
+    }
+  ]
+  res.status(200).json({
+    message: 'Post sent successfully',
+    posts
+  })
 })
 
 module.exports = app
