099e18ee10a7db32f94399c03f460db9ae616b46
apecr <alberto.eyo@gmail.com>
2020-04-22 18:29:44 +0200

    feat() Hash password and save user
    Save users with password hashed and avoid mongoose warning

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express()
 const connectionUri = process.env.MONGODB_URI
 
 mongoose
-  .connect(connectionUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(connectionUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then(() => console.log('Connected to Database'))
   .catch(console.error)
 
@@ -19,8 +23,14 @@ app.use('/images', express.static('images'))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS, PUT'
+  )
   next()
 })
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const User = require('./../models/user')
 
 const router = new express.Router()
 
-router.post('/signup', (req, res, next) => {
-
+router.post('/signup', async(req, res, next) => {
+  const hashPassword = await   bcrypt.hash(req.body.password, 10)
+  const user = new User({
+    email: req.body.email,
+    password: hashPassword
+  })
+  return user.save()
+    .then(result => {
+      res.status(201).json({
+        message: 'User created',
+        user: result
+      })
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 })
 
 module.exports = router
