c02f6a6d42d6202c4ea75c94d679ba0a08b5eee2
apecr <alberto.eyo@gmail.com>
2020-04-23 11:11:04 +0200

    feat(src/routes/user.js) Inlcude login route
    Generate a jsonwebtoken if login goes ok

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,25 +1,61 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('./../models/user')
+
+const secretJWT = process.env.JWT_SECRET
 
 const router = new express.Router()
 
 router.post('/signup', async(req, res, next) => {
-  const hashPassword = await   bcrypt.hash(req.body.password, 10)
+  const hashPassword = await bcrypt.hash(req.body.password, 10)
   const user = new User({
     email: req.body.email,
-    password: hashPassword
+    password: hashPassword,
   })
-  return user.save()
-    .then(result => {
+  return user
+    .save()
+    .then((result) => {
       res.status(201).json({
         message: 'User created',
-        user: result
+        user: result,
       })
     })
-    .catch(error => {
-      res.status(500).json(error)
+    .catch((error) => res.status(500).json(error))
+})
+
+router.post('/login', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(async(user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Auth failed',
+        })
+      }
+      try {
+        const result = await bcrypt.compare(req.body.password, user.password)
+        if (!result) {
+          return res.status(401).json({
+            message: 'Auth failed',
+          })
+        }
+        const token = jwt.sign(
+          { email: user.email, userId: user._id },
+          secretJWT,
+          {
+            expiresIn: '1h',
+          }
+        )
+        return res.status(200).json({
+          message: 'Auth ok',
+          token,
+        })
+      } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+      }
     })
+    .catch((error) => res.status(500).json(error))
 })
 
 module.exports = router
