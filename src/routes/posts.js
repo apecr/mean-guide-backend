944cbcde7fb138705e8abe08749a20ed494ebfd5
apecr <alberto.eyo@gmail.com>
2020-04-27 11:39:23 +0200

    refactor() Create a image middleware
    Pass the image upload middleware to one file

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,6 +1,6 @@
-const multer = require('multer')
 const express = require('express')
 const checkAuth = require('./../middleware/check-auth')
+const { imageProcess } = require('./../middleware/image')
 const {
   createPost,
   editPost,
@@ -9,30 +9,7 @@ const {
   deletePost,
 } = require('./../controllers/posts')
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg',
-}
 const router = new express.Router()
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype]
-    let error = new Error(`Invalid mime type ${file.mimetype}`)
-    if (isValid) {
-      error = null
-    }
-    cb(error, 'images')
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-')
-    const ext = MIME_TYPE_MAP[file.mimetype]
-    cb(null, `${name}-${Date.now()}.${ext}`)
-  },
-})
-
-const imageProcess = multer({ storage }).single('image')
 
 router.post('', checkAuth, imageProcess, createPost)
 router.put('/:postId', checkAuth, imageProcess, editPost)
